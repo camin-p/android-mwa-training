6829b0874d108477a1567d5719707959318ace63
Camin <admin@maxile.net>
2018-08-28 10:10:10 +0700

    add language

--- app/src/main/java/com/maxile/lesson/myapplication2/MainActivity.java
+++ app/src/main/java/com/maxile/lesson/myapplication2/MainActivity.java
@@ -1,15 +1,22 @@
 package com.maxile.lesson.myapplication2;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +35,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        findViewById(R.id.main_btn).setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        Locale current = getResources().getConfiguration().locale;
+        String currentL = current.getLanguage();
+        String lang;
+        if (current.getLanguage()=="th")
+            lang = "en";
+        else
+            lang = "th";
+        MainActivity.this.setLanguage(lang);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int i =0 ;
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        int i =0 ;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        int i =0 ;
+    }
+    public void setLanguage(String language){
+        Resources res = MainActivity.this.getResources();
+
+        DisplayMetrics dm = res.getDisplayMetrics();
+        android.content.res.Configuration conf = res.getConfiguration();
+        conf.setLocale(new Locale(language));
+
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(MainActivity.this,MainActivity.class);
+        refresh.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(refresh);
+        finish();
+    }
 }
